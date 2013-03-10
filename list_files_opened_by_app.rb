1e4a114ae2b01a3537a51e174cad4baa9d4066b5
Greg Ferguson <8si.greg@gmail.com>
2013-03-10 08:46:29 -0700

    added

--- list_files_opened_by_app.rb
+++ list_files_opened_by_app.rb
@@ -1,12 +1,12 @@
 FILENAME = 'trashme.txt'
 
-pid = $$
+pid  = $$
 lsof = []
 
 File.open(FILENAME, 'w') do |fo|
   fo.puts 'This is a test'
-  
+
   IO.popen ("lsof -p #{pid}") { |l| lsof << l.readlines }
-  
+
 end
 puts lsof.select{ |i| i[FILENAME] }
